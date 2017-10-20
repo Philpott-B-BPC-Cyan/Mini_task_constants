0ee5d099e565b601cd2a7a448ffcfa4fac8fcd6e
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-10-20 10:08:45 +0100

    Add files via upload

---
+++ Mini_task_constants.php
@@ -0,0 +1,14 @@
+<?php
+
+define ("Firstname", "Brad",true);
+define ("Lastname", "Philpott", true);
+define ("FavSubject", "Law", true);
+
+$age=17
+
+echo Firstname;
+echo Lastname;
+echo FavSubject;
+echo $age + "5";
+
+?>
